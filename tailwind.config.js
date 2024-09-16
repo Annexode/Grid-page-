d0eaafbe9461c8f9886e46038331579c1b4e1491
Annexode <annexode@gmail.com>
2024-09-16 23:44:21 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dmSans: ["DM Sans"],
+      },
+      colors: {
+        uviolet: ["#7651dc"],
+        uyeloow: ["#ffcc69"],
+        uyeloow2: ["#faeee2"],
+        ublue: ["#dcd1fc"],
+      },
+    },
+  },
+  plugins: [],
+};
